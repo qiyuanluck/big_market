5d95a89f6d4c95836ec2b9882dc5b799b60ab2cf
qiyuan <1184863960@qq.com>
2025-02-18 14:43:04 +0800

    feat: 用户参与抽奖活动库表设计

---
+++ big_market-infrastructure/src/main/java/cn/project/infrastructure/persistent/po/RaffleActivityAccount.java
@@ -0,0 +1,71 @@
+package cn.project.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @Author: qiyuan
+ * @Date: 2025/02/18 14:31
+ * @Description: 抽奖活动账户表 持久化对象
+ */
+@Data
+public class RaffleActivityAccount {
+
+    /**
+     * 自增ID
+     */
+    private Long id;
+
+    /**
+     * 用户ID
+     */
+    private String userId;
+
+    /**
+     * 活动ID
+     */
+    private Long activityId;
+
+    /**
+     * 总次数
+     */
+    private Integer totalCount;
+
+    /**
+     * 总次数-剩余
+     */
+    private Integer totalCountSurplus;
+
+    /**
+     * 日次数
+     */
+    private Integer dayCount;
+
+    /**
+     * 日次数-剩余
+     */
+    private Integer dayCountSurplus;
+
+    /**
+     * 月次数
+     */
+    private Integer monthCount;
+
+    /**
+     * 月次数-剩余
+     */
+    private Integer monthCountSurplus;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+}
+
---
+++ big_market-infrastructure/src/main/java/cn/project/infrastructure/persistent/po/RaffleActivityAccountFlow.java
@@ -0,0 +1,70 @@
+package cn.project.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @Author: qiyuan
+ * @Date: 2025/02/18 14:32
+ * @Description:
+ */
+@Data
+public class RaffleActivityAccountFlow {
+
+    /**
+     * 自增ID
+     */
+    private Integer id;
+
+    /**
+     * 用户ID
+     */
+    private String userId;
+
+    /**
+     * 活动ID
+     */
+    private Long activityId;
+
+    /**
+     * 总次数
+     */
+    private Integer totalCount;
+
+    /**
+     * 日次数
+     */
+    private Integer dayCount;
+
+    /**
+     * 月次数
+     */
+    private Integer monthCount;
+
+    /**
+     * 流水ID - 生成的唯一ID
+     */
+    private String flowId;
+
+    /**
+     * 流水渠道（activity-活动领取、sale-购买、redeem-兑换、free-免费赠送）
+     */
+    private String flowChannel;
+
+    /**
+     * 业务ID（外部透传，活动ID、订单ID）
+     */
+    private String bizId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+}
